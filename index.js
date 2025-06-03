30fb3fb2372ea53832f458ce3f9d873bdce71e4a
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-06-03 11:01:43 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from "express";
-import cors from 'cors'
+import mongoose from "mongoose";
+import cors from "cors";
+
 const app = express();
+
 app.listen(8080, () => {
   console.log("Server Started");
 });
+
 app.use(cors())
 app.get("/", (req, res) => {
   return res.send("Good Morning");
