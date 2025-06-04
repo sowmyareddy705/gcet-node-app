141ba7d7b2a0f0ab2114108e609518671cbf41ba
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-06-04 10:48:58 +0530

    update

--- models/oderModel.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose'
-const orderSchema = mongoose.Schema({
-  email: { type: String },
-  orderValue: { type: Number },
-});
-
-export default mongoose.model("Order", orderSchema);
