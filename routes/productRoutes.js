2b9f6462a76f36f136f8c2cbdadc0403d284b70b
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-06-05 09:33:22 +0530

    update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,25 @@
-import express from 'express'
-import productModel from "../models/productModel.js";
+import express from 'express';
+import productModel from '../models/productModel.js';
 
-const productRouter = express.Router()
+const productRouter = express.Router();
 
-productRouter.get("/all", async (req, res) => {
-  const products = await productModel.find();
-  res.json(products);
+productRouter.get('/', async (req, res) => {
+  try {
+    const products = await productModel.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching products', error });
+  }
 });
 
-export default productRouter
+productRouter.post("/add", async (req, res) => {
+  try {
+    const { name, description, imgUrl, price } = req.body;
+    const newProduct = new productModel({ name, description, imgUrl, price });
+    const result = await newProduct.save();
+    return res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Error adding product", error });
+  }
+});
+export default productRouter;
