392b85466f9e7e8e56f2da42efc1535ae1be4d22
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-06-06 15:35:28 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,8 @@ import express from 'express'
 import userModel from "../models/userModel.js";
 import bcrypt from 'bcryptjs';
 const userRouter = express.Router()
-
+import jwt from "jsonwebtoken";
+const SECRET_KEY = "helloworld";
 userRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
   const hashpassword = await bcrypt.hash(password, 10);
@@ -14,7 +15,9 @@ userRouter.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const result = await userModel.findOne({ email, password });
   if (!result) return res.json({ message: "Invalid user or password" });
+  const token = jwt.sign({ email: result.email, id: result._id}, SECRET_KEY);
   return res.json(result);
+  return res.json({ user: result, token: token});
 });
 
 userRouter.get("/:id", async(req, res)=>{
